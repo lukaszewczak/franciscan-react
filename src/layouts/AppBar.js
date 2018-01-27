c3903f1eb5e8038fe842f57b66d8c6a1928947f9
Łukasz Szewczak <lukaszewczak@gmail.com>
2018-01-27 13:37:25 +0100

    Fix formating

--- src/layouts/AppBar.js
+++ src/layouts/AppBar.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {AppBar as MUIAppBar} from 'material-ui/AppBar'
+import { AppBar as MUIAppBar } from 'material-ui/AppBar'
 import ToolBar from 'material-ui/Toolbar'
 import Typography from 'material-ui/Typography'
 import IconButton from 'material-ui/IconButton'
@@ -10,119 +10,121 @@ import SearchIcon from 'material-ui-icons/Search'
 import CloseIcon from 'material-ui-icons/Close'
 import PrintIcon from 'material-ui-icons/Print'
 import MoreVertIcon from 'material-ui-icons/MoreVert'
-import Menu, {MenuItem} from 'material-ui/Menu'
+import Menu, { MenuItem } from 'material-ui/Menu'
 import classNames from 'classnames'
 import Tooltip from 'material-ui/Tooltip'
 import TextField from 'material-ui/TextField'
 
 const AppBar = ({
-                    classes,
-                    open,
-                    openMenu,
-                    handleAnyInputChange,
-                    handleSearchToggle,
-                    showSearch,
-                    openSearch,
-                    handleDrawerOpen,
-                    handleClick,
-                    anchorEl,
-                    handleRequestClose,
-                    handleLayoutChange,
-                    handleLogout,
-                    handlePrintIcon,
-                    viewType,
-                    style
-                }) => {
-    return (
-        <div style={style}>
-            <MUIAppBar
-                className={classNames(classes.appBar, classes.open)}
+  classes,
+  open,
+  openMenu,
+  handleAnyInputChange,
+  handleSearchToggle,
+  showSearch,
+  openSearch,
+  handleDrawerOpen,
+  handleClick,
+  anchorEl,
+  handleRequestClose,
+  handleLayoutChange,
+  handleLogout,
+  handlePrintIcon,
+  viewType,
+  style
+}) => {
+  return (
+    <div style={style}>
+      <MUIAppBar className={classNames(classes.appBar, classes.open)}>
+        <ToolBar disableGutters={!open}>
+          <IconButton
+            color="primary"
+            aria-label="open drawer"
+            onClick={handleDrawerOpen}
+            className={classNames(classes.menuButton, classes.navIconHide)}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            type="display1"
+            color="inherit"
+            className={classNames(classes.flex, classes.typo)}
+            noWrap
+          >
+            Bulletin
+          </Typography>
+          {showSearch ? (
+            <div>
+              <TextField
+                onChange={handleAnyInputChange}
+                name="searchText"
+                placeholder="Search"
+                autoFocus
+              />
+              <Tooltip title="Close Search" placement="bottom">
+                <IconButton
+                  onClick={handleSearchToggle}
+                  color="primary"
+                  aria-label="Close Search"
+                >
+                  <CloseIcon />
+                </IconButton>
+              </Tooltip>
+            </div>
+          ) : (
+            <Tooltip title="Search" placement="bottom">
+              <IconButton
+                onClick={handleSearchToggle}
+                color="primary"
+                aria-lable="Search"
+              >
+                <SearchIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+
+          <Tooltip
+            title={viewType === 'grid' ? 'List View' : 'Grid View'}
+            placement="bottom"
+          >
+            <IconButton
+              onClick={handleLayoutChange}
+              color="primary"
+              aria-lable="View-type"
             >
-                <ToolBar disableGutters={!open}>
-                    <IconButton
-                        color="primary"
-                        aria-label="open drawer"
-                        onClick={handleDrawerOpen}
-                        className={classNames(classes.menuButton, classes.navIconHide)}
-                    >
-                        <MenuIcon/>
-                    </IconButton>
-                    <Typography
-                        type="display1"
-                        color="inherit"
-                        className={classNames(classes.flex, classes.typo)}
-                        noWrap>
-                        Bulletin
-                    </Typography>
-                    {showSearch ? (
-                        <div>
-                            <TextField
-                                onChange={handleAnyInputChange}
-                                name="searchText"
-                                placeholder="Search"
-                                autoFocus
-                            />
-                            <Tooltip title="Close Search"
-                                     placement="bottom">
-                                <IconButton
-                                    onClick={handleSearchToggle}
-                                    color="primary"
-                                    aria-label="Close Search">
-                                    <CloseIcon/>
-                                </IconButton>
-                            </Tooltip>
-                        </div>
-
-                    ) : (
-                        <Tooltip title="Search"
-                                 placement="bottom">
-                            <IconButton
-                                onClick={handleSearchToggle}
-                                color="primary"
-                                aria-lable="Search">
-                                <SearchIcon/>
-                            </IconButton>
-                        </Tooltip>
-                    )
-                    }
-
-                    <Tooltip
-                        title={viewType === 'grid' ? 'List View' : 'Grid View'}
-                        placement="bottom">
-                        <IconButton
-                            onClick={handleLayoutChange}
-                            color="primary"
-                            aria-lable="View-type">
-                            {viewType === 'grid' ? <ViewStreamIcon/> : <ViewQuiltIcon/>}
-                        </IconButton>
-                    </Tooltip>
-                    <Tooltip title="Print View">
-                        <IconButton
-                            color="primary"
-                            aria-label="Print View"
-                            onClick={handlePrintIcon}>
-                            <PrintIcon/>
-                        </IconButton>
-                    </Tooltip>
-                    <IconButton
-                        color="primary"
-                        aria-label="More"
-                        aria-owns={openMenu ? 'simple-menu' : null}
-                        aria-haspopup="true"
-                        onClick={handleClick}>
-                        <MoreVertIcon/>
-                    </IconButton>
-                    <Menu
-                        id="simple-menu"
-                        anchorEl={anchorEl}
-                        open={openMenu}
-                        onRequestClose={handleRequestClose}>
-                        <MenuItem onClick={handleLogout}>Logout</MenuItem>
-                    </Menu>
-                </ToolBar>
-            </MUIAppBar>
-        </div>
-    )
+              {viewType === 'grid' ? <ViewStreamIcon /> : <ViewQuiltIcon />}
+            </IconButton>
+          </Tooltip>
+          <Tooltip title="Print View">
+            <IconButton
+              color="primary"
+              aria-label="Print View"
+              onClick={handlePrintIcon}
+            >
+              <PrintIcon />
+            </IconButton>
+          </Tooltip>
+          <IconButton
+            color="primary"
+            aria-label="More"
+            aria-owns={openMenu ? 'simple-menu' : null}
+            aria-haspopup="true"
+            onClick={handleClick}
+          >
+            <MoreVertIcon />
+          </IconButton>
+          <Menu
+            id="simple-menu"
+            anchorEl={anchorEl}
+            open={openMenu}
+            onRequestClose={handleRequestClose}
+          >
+            <MenuItem onClick={handleLogout}>Logout</MenuItem>
+          </Menu>
+        </ToolBar>
+      </MUIAppBar>
+    </div>
+  )
 }
 
 export default AppBar
