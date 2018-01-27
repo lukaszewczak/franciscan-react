343448a415d6089229188ae8e29871d7444db5f5
Łukasz Szewczak <lukaszewczak@gmail.com>
2018-01-27 12:52:21 +0100

    Add appbar component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,45 @@
 import React from 'react'
+import {withStyles, MuiThemeProvider} from 'material-ui/styles'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 
-import Header from '../components/Header'
+import AppBar from './AppBar'
 import './index.css'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
+const TemplateWrapper = ({children}) => (
+    const classes = this.props.classes
+    <MuiThemeProvider theme={theme}>
+        <div>
+            <Helmet
+                title="Gatsby Default Starter"
+                meta={[
+                    {
+                        name: 'description',
+                        content: 'Sample'
+                    },
+                    {
+                        name: 'keywords',
+                        content: 'sample, something'
+                    },
+                ]}
+            />
+            <AppBar/>
+            <div
+                style={{
+                    margin: '0 auto',
+                    maxWidth: 960,
+                    padding: '0px 1.0875rem 1.45rem',
+                    paddingTop: 0,
+                }}
+            >
+                {children()}
+            </div>
+        </div>
+    </MuiThemeProvider>
 )
 
 TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+    children: PropTypes.func,
 }
 
 export default TemplateWrapper
